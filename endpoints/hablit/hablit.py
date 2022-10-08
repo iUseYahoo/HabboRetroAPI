c76d7372ce4d46a303a499aa3a595057ec6530c0
iUseYahoo <logicatedlzyt@gmail.com>
2022-10-08 13:18:11 +1100

    Display unicode characters in user motto

--- endpoints/hablit/hablit.py
+++ endpoints/hablit/hablit.py
@@ -4,6 +4,7 @@ import requests
 
 def hablit_search(username):
     url = "https://hablit.net/profile/{}".format(username)
+    # This expires after a certain amount of time or uses I belive, Or maybe even if the wrong IP is using it, so try your own cookie.
     r = requests.get(url, cookies={'hablit_session': 'eyJpdiI6IkYzcmlqY1BSRzlHWC82WTNMSVVQU2c9PSIsInZhbHVlIjoiYVoyVjVDTTZPNkhSSEZ6VVdqYmUzTTVlUG9PYkdwSDFTSS80Z2J1RFR0MzFtTW5SOVZBY0JqMCtpSGk5V1hzL2hieVIrN20wZ2JnamFKN3VONWlHRkJNV3ZMVGh2cWJITFBmYkR5K2piVy9DUTc2QUhaMStMb2c1RDVuU2xuQW8iLCJtYWMiOiI3Y2I1ZDBhMDIwZmQxNDhhZjY0ODk0N2RhMTlkYzYxNTlhYWY5NTYzY2QyNWUzZjc1ZTMwMDU1NzljZDllZDM3IiwidGFnIjoiIn0%3D'})
     soup = BeautifulSoup(r.text, "html.parser")
 
@@ -43,9 +44,13 @@ def hablit_search(username):
     #     diamonds = "Diamonds are not set on webpage."
 
 
-    raw_data = "Avatar: {}\nMotto: {}\nCoins: {}\nDuckets: {}\nDiamonds: {}".format(avatar, motto, coins, duckets, diamonds)
-    print(raw_data)
+    # this is used for debugging
+    # raw_data = "Avatar: {}\nMotto: {}\nCoins: {}\nDuckets: {}\nDiamonds: {}".format(avatar, motto, coins, duckets, diamonds)
+    # print(raw_data)
     
+    # display unicode for motto
+    motto = motto.encode('ascii', 'xmlcharrefreplace').decode('utf-8')
+
     return json.dumps({
         "avatar": avatar.strip('\r\n').strip("\r\n "),
         "motto": motto.strip('\r\n').strip("\r\n "),
